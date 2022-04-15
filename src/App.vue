4b89c56e0eeec77bcffab2cd0564e88e0367c3e4
beyazskorsky <hhsynalv@gmail.com>
2022-04-16 00:02:17 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+    <hr>
+    <app-new-product></app-new-product>
+    <br><br>
+    <app-progress></app-progress>
+    <br><br>
+    <app-products></app-products>
+
+  </div>
+
+
+</template>
+
+<script>
+
+import NewProduct from "./components/NewProduct";
+import Progress from "./components/Progress";
+import Products from "./components/Products";
+
+  export default {
+    components : {
+      appNewProduct : NewProduct,
+      appProgress : Progress,
+      appProducts : Products
+    },
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  body {
+    background-color: aliceblue;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+
+
+</style>
+
---
+++ src/components/Progress.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="progress" v-if="length != null">
+    <div class="progress-bar bg-info" role="progressbar" :style="{width : progress + '%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+      {{ length }}/10
+    </div>
+  </div>
+</template>
+
+<script>
+import {eventBus} from "../main";
+
+export default {
+  name: "Progress",
+  data(){
+    return{
+      length : null,
+      progress : null
+    }
+  },
+  created() {
+    eventBus.$on("progressBarUpdated",(length)=>{
+      this.length = length;
+      this.progress = length * 10;
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
